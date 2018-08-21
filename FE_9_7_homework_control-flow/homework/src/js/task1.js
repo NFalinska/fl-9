c9b64dee25234df88f0aba5da26857f696de1087
NFalinska <nataliia.falinska@gmail.com>
2018-08-21 21:19:28 +0300

    add task1

--- FE_9_7_homework_control-flow/homework/src/js/task1.js
+++ FE_9_7_homework_control-flow/homework/src/js/task1.js
@@ -1,1 +1,32 @@
-// Your code goes here
+function registration(){
+	let login = prompt('Please write your login');
+    if (login==='') {
+    alert('Canceled')
+	return false;
+    } else if (login.length<4) {
+	alert(' I dont know any users having name length less than 4 symbols')
+	return false;
+	} else if (login!==''&&login!=='User') {
+	alert('I dont know you')
+	return false;
+	}
+
+	let password = prompt('Please write your password');
+	let now = new Date().getHours();
+	switch(password){
+		case '':
+		alert('Canceled');
+		break;
+		case 'SuperUser':
+			if(now > 20) {
+				alert('Good evening!');
+			} else {
+				alert('Good day!');
+			}
+		break;
+		default:
+		alert('Wrong password');
+		break;
+	}
+}
+registration();
