f7e6c610792ab086e7d775d7a3c49f96109e9057
NFalinska <nataliia.falinska@gmail.com>
2018-08-28 23:02:32 +0300

    add hw9

--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -1,1 +1,28 @@
-// Your code goes here
+function findType(a) {
+    return typeof a;
+}
+
+function forEach(arr, fn) {
+
+    for (let i = 0; i < arr.length; i++) {
+        fn(arr[i]);
+    }
+}
+
+function map(arr, fn) {
+    let newArray = [];
+    forEach(arr, function(el) {
+        newArray.push(fn(el));
+    });
+    return newArray;
+}
+
+function filter(arr, fn) {
+    let newArray = [];
+    forEach(arr, function(el) {
+        if (fn(el)) {
+            newArray.push(el);
+        }
+    });
+    return newArray;
+}
