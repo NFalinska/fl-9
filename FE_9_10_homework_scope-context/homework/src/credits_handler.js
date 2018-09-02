48156b9e8a3a30b28d1026d9623612b5a9c42d89
NFalinska <nataliia.falinska@gmail.com>
2018-09-02 22:39:50 +0300

    add hw10

--- FE_9_10_homework_scope-context/homework/src/credits_handler.js
+++ FE_9_10_homework_scope-context/homework/src/credits_handler.js
@@ -1,1 +1,32 @@
-/* Your code goes here */
+function userCard(key) {
+    let balance = 100;
+    let transactionLimit = 100;
+    let historyLogs = [];
+    let time = new Date().toLocaleString('en-GB');
+
+
+    function historyLog(operationType, credits, operationTime) {
+        historyLogs.push({
+            operationType: operationType,
+            credits: credits,
+            operationTime: operationTime
+        })
+    }
+
+    function putCredits(value) {
+       balance += value;
+    }
+
+    function takeCredits(value) {
+        if (value < transactionLimit) {
+            balance -= value;
+        } else {
+            console.log('The value is bigger than your limit')
+        }
+    }
+    function setTransactionLimit(value) {
+        transactionLimit += value;
+    }
+}
+
+
--- FE_9_9_homework_data-types/homework/index.js
+++ FE_9_9_homework_data-types/homework/index.js
@@ -1,40 +1,46 @@
-function findType(a) {
-    return typeof a;
-}
+//function findType(a) {
+//    return typeof a;
+//}
+//
+//function forEach(arr, fn) {
+//
+//    for (let i = 0; i < arr.length; i++) {
+//        fn(arr[i]);
+//    }
+//}
+//
+//function map(arr, fn) {
+//    let newArray = [];
+//    forEach(arr, function(el) {
+//        newArray.push(fn(el));
+//    });
+//    return newArray;
+//}
+//
+//function filter(arr, fn) {
+//    let newArray = [];
+//    forEach(arr, function(el) {
+//        if (fn(el)) {
+//            newArray.push(el);
+//        }
+//    });
+//    return newArray;
+//}
+//
+//function keys(obj){
+//	return Object.keys(obj);
+//}
+//
+//function values(obj) {
+//	return Object.values(obj);
+//}
+//
+//function showFormattedDate(date) {
+//	return `It is ${date.getDate()} of ${date.toLocaleString('en-US',{month: 'short'})}, ${date.getFullYear()} `
+//}
 
-function forEach(arr, fn) {
+//if (function f(){}) {
+//  alert(typeof f);
+//}
 
-    for (let i = 0; i < arr.length; i++) {
-        fn(arr[i]);
-    }
-}
-
-function map(arr, fn) {
-    let newArray = [];
-    forEach(arr, function(el) {
-        newArray.push(fn(el));
-    });
-    return newArray;
-}
-
-function filter(arr, fn) {
-    let newArray = [];
-    forEach(arr, function(el) {
-        if (fn(el)) {
-            newArray.push(el);
-        }
-    });
-    return newArray;
-}
-
-function keys(obj){
-	return Object.keys(obj);
-}
-
-function values(obj) {
-	return Object.values(obj);
-}
-
-function showFormattedDate(date) {
-	return `It is ${date.getDate()} of ${date.toLocaleString('en-US',{month: 'short'})}, ${date.getFullYear()} `
-}
+console.log(0 || 0 && 2 || 3 )
